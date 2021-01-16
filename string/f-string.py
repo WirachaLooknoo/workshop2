57479bbf62de1f500ab3416d5b06d21f993fafe7
WirachaLooknoo <pedddna@gmail.com>
2021-01-17 02:28:29 +0700

    exercise.

--- string/f-string.py
+++ string/f-string.py
@@ -3,4 +3,4 @@ age = 21
 
 result = f" My name is {name}, and I am {age}"
 
-print("result", result) # Output: "My name is Jim, and I am 20"
+print("result", result) # Output: "My name is Looknoo, and I am 20"
