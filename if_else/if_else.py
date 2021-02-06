d467ec834bb01755ce3b839cd8f34c44e72c2401
WirachaLooknoo <pedddna@gmail.com>
2021-02-06 23:31:59 +0700

    exercise.

---
+++ if_else/if_else.py
@@ -0,0 +1,19 @@
+grade = int(input("Enter your point : "))
+
+if grade <= 49:
+    print("F")
+elif grade <= 54:
+    print("D")
+elif grade <= 59:
+    print("D+")
+elif grade <= 64:
+    print("C")
+elif grade <= 69:
+    print("C+")
+elif grade <= 74:
+    print("B")
+elif grade <= 79:
+    print("B+")
+elif grade <= 100:
+    print("A")
+
